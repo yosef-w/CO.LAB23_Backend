f8430c5ca43e39a96adeccc09cd5980392c7fbde
traviicii <125103256+traviicii@users.noreply.github.com>
2023-10-04 22:13:42 -0400

    Flask is currently running. Tentatively completed User model. Made many adjustments to main init file and had to adjust requirements to previous versions of Flask and Werzeug as there were incompatible methods between the two fo them.

--- config.py
+++ config.py
@@ -2,7 +2,9 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+class Config():
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_SIZE = 3
